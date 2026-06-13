67ec94bf82266a97e9c69a6ad5bca55b6c2d6a58
Kévin Alexandre Boissonneault <kaboissonneault@gmail.com>
2026-06-12 23:15:47 -0400

    Added project types to Tiled data. Data-driving player spawn point

--- src/plugins/game/mod.rs
+++ src/plugins/game/mod.rs
@@ -1,7 +1,8 @@
 mod tile_entity_transform;
 
+use std::ops::Deref;
 use bevy::prelude::*;
-use bevy_ecs_tiled::prelude::{TiledMap, TilemapAnchor};
+use bevy_ecs_tiled::prelude::{MapCreated, TilePos, TiledEvent, TiledMap, TiledMapStorage, TiledObject, TilemapAnchor};
 
 const TILE_COORD_TO_WORLD_POS: f32 = 24.0;
 
@@ -10,12 +11,16 @@ fn get_tile_center_world_coord(tile_coord: IVec2) -> Vec2 {
         + Vec2::new(TILE_COORD_TO_WORLD_POS/2.0, TILE_COORD_TO_WORLD_POS/2.0)
 }
 
-#[derive(Component, Default)]
+#[derive(Component, Default, Clone, Copy, PartialEq, Debug)]
 struct TileCoord(IVec2);
 
 impl TileCoord {
     fn center_world_coord(&self) -> Vec2 {
         get_tile_center_world_coord(self.0)
+    }
+
+    fn from_world_coord(world_coord: Vec2) -> TileCoord {
+        TileCoord((world_coord / TILE_COORD_TO_WORLD_POS).as_ivec2())
     }
 }
 
@@ -27,39 +32,55 @@ enum SpawnPoint {
     Player,
 }
 
-#[derive(Resource)]
+#[derive(Resource, Default)]
 struct SpawnPoints {
-    player_points: Vec<Entity>
+    player_points: Vec<Entity>,
+    player_point_coords: Vec<TileCoord>,
 }
 
 pub(crate) fn plugin(app: &mut App) {
     app.add_plugins(tile_entity_transform::plugin);
 
     app.insert_resource(TestEntityAssets::default());
-    app.add_systems(Startup, (orc_thief_setup,
-                              (load_title_entity_assets, spawn_player).chain()));
+    app.insert_resource(SpawnPoints::default());
+    app.add_systems(Startup, (load_test_entity_assets, orc_thief_setup).chain());
     app.add_systems(Update, move_player);
+
     app.register_type::<SpawnPoint>();
+    app.add_observer(on_add_spawn);
 }
 
 fn orc_thief_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let map = asset_server.load("maps/map1.tmx");
     commands.spawn((TiledMap(map), TilemapAnchor::Center))
-        .observe(on_add_spawn);
+        .observe(on_map_created);
 }
 
-fn on_add_spawn(add_spawn: On<Add, SpawnPoint>, spawn_query: Query<&SpawnPoint>, mut spawn_points: ResMut<SpawnPoints>) {
+fn on_add_spawn(add_spawn: On<Add, SpawnPoint>, spawn_query: Query<(&SpawnPoint, &GlobalTransform)>, mut spawn_points: ResMut<SpawnPoints>) {
     let spawn_entity = add_spawn.event().entity;
 
-    let spawn_type = spawn_query.get(spawn_entity).unwrap();
+    let (spawn_type, spawn_world_transform) = spawn_query.get(spawn_entity).unwrap();
 
     match spawn_type {
         SpawnPoint::Player => {
             spawn_points.player_points.push(spawn_entity);
+            spawn_points.player_point_coords.push(TileCoord::from_world_coord(spawn_world_transform.translation().truncate()));
         },
         SpawnPoint::Unknown => {
             warn!("Unknown spawn type");
         }
+    }
+}
+
+fn on_map_created(_map_created: On<TiledEvent<MapCreated>>
+                  , spawn_points: Res<SpawnPoints>
+                  , mut commands: Commands
+                  , assets: Res<TestEntityAssets>)
+{
+    if let Some(spawn_point) = spawn_points.player_point_coords.first() {
+        commands.spawn((Player, *spawn_point, Mesh2d(assets.mesh.clone()), MeshMaterial2d(assets.material.clone())));
+    } else {
+        warn!("No player spawn point found");
     }
 }
 
@@ -72,7 +93,7 @@ struct TestEntityAssets {
 const TEST_ENTITY_SHAPE: Circle = Circle::new(12.0);
 const TEST_ENTITY_COLOR: Color = Color::srgb(0.6, 0.4, 0.4);
 
-fn load_title_entity_assets(mut meshes: ResMut<Assets<Mesh>>
+fn load_test_entity_assets(mut meshes: ResMut<Assets<Mesh>>
                             , mut materials: ResMut<Assets<ColorMaterial>>
                             , mut assets: ResMut<TestEntityAssets>)
 {
@@ -88,11 +109,6 @@ struct Character;
 #[derive(Component, Default)]
 #[require(Character)]
 struct Player;
-
-fn spawn_player(mut commands: Commands
-                     , assets: Res<TestEntityAssets>) {
-    commands.spawn((Player, Mesh2d(assets.mesh.clone()), MeshMaterial2d(assets.material.clone())));
-}
 
 fn move_player(mut player: Single<&mut TileCoord, With<Player>>
     , input: Res<ButtonInput<KeyCode>>) {
--- src/third_party/bevy_ecs_tiled.rs
+++ src/third_party/bevy_ecs_tiled.rs
@@ -5,12 +5,12 @@ use bevy_ecs_tiled::prelude::*;
 pub(super) fn plugin(app: &mut App) {
     let mut export_path : PathBuf = PathBuf::new();
     export_path.push("exports");
-    export_path.push("properties_basic_types.json");
+    export_path.push("orc_thief_types.json");
     app.add_plugins(TiledPlugin(TiledPluginConfig {
         tiled_types_export_file: Some(export_path),
         tiled_types_filter: TiledFilter::from(
             regex::RegexSet::new([
-                r"^properties_basic::.*",
+                r"^orc_thief_bevy::.*",
                 r"^bevy_sprite::text2d::Text2d$",
                 r"^bevy_text::text::TextColor$",
                 r"^bevy_ecs::name::Name$",
